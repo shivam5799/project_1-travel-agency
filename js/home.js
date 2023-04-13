30f30d28f338aab2df3ff23b657660e92d0521ea
shivam5799 <115656331+shivam5799@users.noreply.github.com>
2023-04-13 11:15:50 +0530

    first commit

---
+++ js/home.js
@@ -0,0 +1,24 @@
+
+	 $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('black');
+            }
+      })
+
+
+       $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+       
+
+
+
+
+     
